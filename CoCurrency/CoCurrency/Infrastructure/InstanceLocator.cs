e2013eb404fd65d4d4a9ff6062c4a84d5800c066
JUAN FERRERAS <jferrerasdiaz12@hotmail.com>
2019-01-02 17:33:36 -0400

    Login Page

---
+++ CoCurrency/CoCurrency/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoCurrency.Infrastructure
+{
+    using ViewModels;
+
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
